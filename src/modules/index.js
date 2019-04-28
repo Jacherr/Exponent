502f3284a76a0b68204504860d57d8c8d9e94c9f
Jacher <jwc2002@outlook.com>
2019-04-28 15:07:16 +0100

    update to account for removed modules

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,3 @@
 export { default as Core } from './Core';
 export { default as Private } from './Private';
-export { default as Announcement } from './Announcement';
-export { default as Moderation } from './Moderation';
 
