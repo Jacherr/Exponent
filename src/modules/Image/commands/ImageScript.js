0fce125c2f1c092b5c802b09d86e232bf8a04bc8
Jacher <jwc2002@outlook.com>
2019-05-05 17:49:06 +0100

    add imagescript support

---
+++ src/modules/Image/commands/ImageScript.js
@@ -0,0 +1,57 @@
+import { Command } from 'axoncore';
+const superagent = require('superagent')
+const apikeys = require('../../../configs/tokenConf.json')
+
+class ImageScript extends Command {
+    constructor(module) {
+        super(module);
+
+        this.label = 'imagescript';
+        this.aliases = [
+            'runis',
+            'is',
+            'imgscript'
+        ];
+
+        this.hasSubcmd = false;
+
+        this.infos = {
+            owner: ['Jacher'],
+            name: 'imagescript',
+            description: 'Run imagescript, documented at https://gist.github.com/matmen/d4fa000110efe2944078fb8065dafd11#file-imagescript-md',
+            usage: 'imagescript [args]',
+            examples: ['imagescript load https://funnymemes.com/funnymeme.png image\nexplode image\nrender image'],
+        };
+
+        this.options.argsMin = 1;
+        this.options.cooldown = 5000;
+        this.options.guildOnly = false;
+    }
+
+    async execute( { msg, args } ) {
+        let start = Date.now()
+        let message = await this.sendMessage(msg.channel, 'Processing...')
+        superagent
+            .post(`https://fapi.wrmsr.io/imagetagparser`)
+            .set({
+                Authorization: apikeys.apis.fapi,
+                "Content-Type": "application/json"
+            })
+            .send({
+                args: {
+                    text: args,
+                }
+            })
+            .end((err, response) => {
+                if (err) {
+                    message.edit(`${err.toString()}`);
+                }
+                else {
+                    message.delete()
+                    msg.channel.createMessage(`${Date.now() - start}ms`, { file: response.body, name: `imagescript.png` })
+                };
+            });
+    }
+}
+
+export default ImageScript;
--- src/modules/Image/commands/index.js
+++ src/modules/Image/commands/index.js
@@ -1,3 +1,4 @@
 export { default as EmojiMosaic } from './EmojiMosaic';
 export { default as Magik } from './Magik';
 export { default as GMagik } from './GMagik';
+export { default as ImageScript } from './ImageScript'
