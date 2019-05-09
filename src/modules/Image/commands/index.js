595ac90c5ac272b7563a14d40ef938e25fb7bbc8
Jacher <jwc2002@outlook.com>
2019-05-09 17:41:38 +0100

    oops

--- src/modules/Image/commands/index.js
+++ src/modules/Image/commands/index.js
@@ -2,4 +2,4 @@ export { default as EmojiMosaic } from './EmojiMosaic';
 export { default as Magik } from './Magik';
 export { default as GMagik } from './GMagik';
 export { default as ImageScript } from './ImageScript';
-export { default as Magik } from './Polarise';
+export { default as Polarise } from './Polarise';
