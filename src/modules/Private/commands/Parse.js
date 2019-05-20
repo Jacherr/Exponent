745a857f2d9cde74158cd2693e900ca04639ed2c
Jacher <jwc2002@outlook.com>
2019-05-20 16:55:46 +0100

    parse

---
+++ src/modules/Private/commands/Parse.js
@@ -0,0 +1,33 @@
+import { Command } from 'axoncore';
+
+class Parse extends Command {
+    constructor(module) {
+        super(module);
+
+        this.label = 'parse';
+        this.aliases = ['p'];
+
+        this.infos = {
+            owner: ['Jacher'],
+            name: 'parse',
+            description: 'Parse a string',
+            usage: 'parse [string]',
+            examples: ['parse {e}'],
+        };
+
+        this.options.argsMin = 1;
+        this.options.cooldown = null;
+
+        this.permissions.staff.needed = this.axon.staff.owners;
+        this.permissions.staff.bypass = this.axon.staff.owners;
+    }
+  
+    async execute( {
+        msg, args,
+    } ) {
+        let result = this.axon.Parser.parse(args.join(' '), '', '').then(a => a.result)
+        this.sendMessage(result)
+    }
+}
+
+export default Parse;
--- src/modules/Private/commands/index.js
+++ src/modules/Private/commands/index.js
@@ -1,2 +1,3 @@
 export { default as Eval } from './Eval';
-export { default as Todo } from './Todo'
+export { default as Todo } from './Todo';
+export { default as Parse } from './Parse';
