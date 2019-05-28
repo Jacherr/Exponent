a925a6a31885b36d36d8b2eaca9e9625759382c6
Jacher <jwc2002@outlook.com>
2019-05-28 11:46:15 +0100

    a

--- src/modules/Private/commands/Todo.js
+++ src/modules/Private/commands/Todo.js
@@ -51,7 +51,7 @@ class Todo extends Command {
         if(flags.map(i => i.flagName).includes('t')) {
             title = flags.find(i => i.flagName == 't').flagContent
             let partA = strArgs.replace(/--t /g, "")
-            let partB = partA.replace(new RegExp(`/${flags.find(i => i.flagName == 't').flagContent, ""}/`, `g`), "")
+            let partB = partA.replace(new RegExp(`/${flags.find(i => i.flagName == 't').flagContent, ""} /`, `g`), "")
             strArgs = partB
         }
         let color
