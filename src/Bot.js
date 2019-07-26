9b53d9c415ac045bf2fca923318d42f3a35204cb
Jacher <jwc2002@outlook.com>
2019-07-26 21:52:02 +0100

    reaction

--- src/Bot.js
+++ src/Bot.js
@@ -45,6 +45,11 @@ client.on('messageCreate', async (msg) => {
             }
         }
     }
+    if(msg.channel.id == '560594334408245248') {
+        client.guilds.get('419317748963999744').emotes.filter(i => i.name.includes('exclamation')).forEach(e => {
+            msg.addReaction(`${e.name}:${e.id}`, '@me')
+        })
+    }
 })
 
 export default Bot;
